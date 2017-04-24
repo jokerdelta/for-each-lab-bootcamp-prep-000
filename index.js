28bd850007e9883333c91344f04e5c66c4585274
Jerry Boyce <jokerdelta@yahoo.com>
2017-04-24 00:19:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var array = [];
+
+function iterativeLog(array){
+
+  array.forEach(console.log (${index} : ${element}));
+
+} // end of function iterativeLog
+
+  function iterate(){
+    
+    var array2 = ["cat", "dog"];
+
+    array2.forEach() // passing the callback to .forEach??
+
+    return array2;
+    
+  } // end of function iterate
+
+function doToArray(array, callback){
+
+  array.forEach();//passing the callback as the forEach callback?
+}
+
+
+  
